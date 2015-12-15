b9d1d6472d0460c6527b0862037c8bf639ebf43f
Jan Karásek <devel@hkar.eu>
2015-12-15 10:59:42 +0100

    added header metadata

--- downloader.py
+++ downloader.py
@@ -3,6 +3,7 @@ import subprocess
 import glob
 import os
 import argparse
+import json
 
 # define args
 parser = argparse.ArgumentParser(description='Download GDELT projects data and filter them.')
@@ -11,16 +12,18 @@ parser.add_argument('--url', type=bool, default=False, help='include url to geo 
 args = parser.parse_args()
 
 os.chdir("./data")
-subprocess.call("echo '' > result.csv", shell=True)  # erase old result file
+subprocess.call("cat /dev/null > result.csv", shell=True)  # erase old result file
 GDELT_DATA_ENDPOINT = 'http://data.gdeltproject.org/events/'  # page with gdelt data index
 GDELT_GEO_FIELDNAMES = [0, 1, 7, 17, 30, 31, 32, 33, 34, 53, 54]  # id's of columns for geo export
+GDELT_HEADER = list(range(0, 58))  # id's of columns for geo export
 gdelt_files_downloaded = glob.glob("./*")
 gdelt_files = []
 
 # download and process
 for line in str(requests.get(GDELT_DATA_ENDPOINT + 'filesizes').text).splitlines():
+    line = line.split(" ")
     if line[1] == 'GDELT.MASTERREDUCEDV2.1979-2013.zip': continue
-    gdelt_files.append(line.split(" "))
+    gdelt_files.append(line)
 
 for file in gdelt_files:
     filename = file[1]
@@ -37,21 +40,19 @@ for file in gdelt_files:
     filename = [x for x in list(glob.glob("*.csv") + glob.glob("*.CSV")) if x not in csv_before][0]
 
     if args.geo:
-        with open("result.csv", "a") as result_file:
-            with open(filename) as f:
-                required_len = len(GDELT_GEO_FIELDNAMES)
+        GDELT_HEADER = GDELT_GEO_FIELDNAMES
+        if args.url: GDELT_HEADER.append(57)
 
-                for line in f:
-                    line = line.split("\t")
-                    result_line = [line[key] for key in GDELT_GEO_FIELDNAMES if len(line[key]) > 0]
+    with open("result.csv", "a") as result_file:
+        result_file.write(json.dumps(GDELT_HEADER) + '\n')
+        with open(filename) as f:
+            required_len = len(GDELT_HEADER)
 
-                    if len(result_line) == required_len:
-                        if args.url and 57 in line:
-                            result_line.append(line[57])
+            for line in f:
+                line = line.replace("\n", '').split("\t")
+                result_line = [(line[key] if key < len(line) else '') for key in GDELT_HEADER]
 
-                        result_file.write(",".join(result_line) + "\n")
-
-    else:
-        subprocess.call("cat %s >> result.csv" % filename, shell=True)
+                if len(result_line) == required_len:
+                    result_file.write(",".join(result_line) + "\n")
 
     subprocess.call("rm %s" % filename, shell=True)
